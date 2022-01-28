79b29aeac0f0385538a4c84ecf9d3f2dfe68e475
SurajGore <suraj@intelehealth.org>
2022-01-28 14:55:14 +0530

    commit

--- 28012022/canvas.js
+++ 28012022/canvas.js
@@ -1,13 +1,13 @@
-function drawDaigonal (){
-    var canvas = document.getElementById('diagonal');
-    var context = canvas.getContext("2d");
+// function drawDaigonal (){
+//     var canvas = document.getElementById('diagonal');
+//     var context = canvas.getContext("2d");
 
-    //begin Pathname
-    context.beginPath();
-    context.moveTo(100, 100);
-    context.lineTo(500, 300);
-    context.stroke(); 
-}
+//     //begin Pathname
+//     context.beginPath();
+//     context.moveTo(100, 100);
+//     context.lineTo(500, 300);
+//     context.stroke(); 
+// }
 
 function drawDaigonal(){
     var canvas = document.getElementById('daigonal');
