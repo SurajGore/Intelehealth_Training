d63d1a4183dbe2094cce400dbf073736d2c7722a
SurajGore <suraj@intelehealth.org>
2022-03-03 17:44:52 +0530

    commit

--- Node Training/03032022/clientMyModule.js
+++ Node Training/03032022/clientMyModule.js
@@ -1,8 +1,6 @@
 // var moduleRef = require('./myModule');
 // console.log(moduleRef.sayHello("bitcode"));
 // console.log(moduleRef.sumAll(1, 2 ,3));
-// console.log(moduleRef.addition(90,67));
-// console.log(moduleRef.addition(90,57,26,45));
 
 
 
@@ -20,8 +18,8 @@
 //     return o;
 // }
 
-// var p1 = createPattern("Suraj","suraj@gmail.com",8855941441);
-// var p1 = createPattern("bitcode","test@gmail.com",8358453454);
+// var p1 = createPattern("Suraj","Suraj@gmail.com",8855941441);
+// var p1 = createPattern("bitcode","test@gmail.com",9562466236);
 // console.log(p1.name === p2.name);
 // console.log(p1.sayHello === p2.sayHello);
 
@@ -40,7 +38,7 @@
 // }
 
 // var p1 = new Person("Suraj","suraj@gmail.com",8855941441);
-// var p2 = new Person("bitcode","test@gmail.com",9090906466);
+// var p2 = new Person("bitcode","test@gmail.com",1566324155);
 // console.log(p1.name === p2.name);
 // console.log(p1.sayHello === p2.sayHello);
 
@@ -48,8 +46,8 @@
 // Prototype Pattern
 function Person(){};
 Person.prototype.name = "Suraj";
-Person.prototype.email = "suraj@gmail.com";
-Person.prototype.tel = 9582300883;
+Person.prototype.email = "Suraj@gmail.com";
+Person.prototype.tel = 8855941441;
 Person.prototype.friends = ["Suraj", "Siddik"];
 Person.prototype.sayHello = function()
 {
@@ -58,30 +56,32 @@ Person.prototype.sayHello = function()
 var p1=new Person();
 p1.name="new name";
 p1.friends.push("Sana");
-console.log
-var p2=createPerson("suraj","suraj@gmail.com",8855941441);
-console.log(p1.name===p2.name); 
-console.log(p1.sayHello===p2.sayHello);
-
-//Prototye Pattern
-// function Person(name,email,tel)
-// {
-//     this.name = name;
-//     this.email = email;
-//     this.tel = tel;
-//     this.friends = ["Suraj", "Siddik"];
-// }
-// Person.prototype = 
-// {
-//     sayHello: function()
-//     {
-//         return "Hello " + this.name;
-//     }
-// }
-// var p1 = new Person("bitcode","test@gmail.com",2374629364);
-// var p2 = new Person("Suraj","suraj@gmail.com",8855941441);
-// p1.friends.push("sana");
-// console.log(p1.friends);
-// console.log(p2.friends);
+console.log(p1.sayHello());
+var p2 = new Person();
+console.log(p2.sayHello());
+console.log(p2.friends);
 // console.log(p1.friends === p2.friends);
 // console.log(p1.sayHello === p2.sayHello);
+
+// Constructor Pattern
+function Person(name,email,tel)
+{
+    this.name = name;
+    this.email = email;
+    this.tel = tel;
+    this.friends = ["Suraj", "Siddik"];
+}
+Person.prototype = 
+{
+    sayHello: function()
+    {
+        return "Hello " + this.name;
+    }
+}
+var p1 = new Person("bitcode","test@gmail.com",2374629364);
+var p2 = new Person("Suraj","suraj@gmail.com",8855941441);
+p1.friends.push("sana");
+console.log(p1.friends);
+console.log(p2.friends);
+console.log(p1.friends === p2.friends);
+console.log(p1.sayHello === p2.sayHello);
